999d4e1972485217a440e6f34df35e58f0600db8
Gabriel da Silva Queiroz <gabriel.squeiroz@a.ficr.edu.br>
2021-11-04 22:05:36 -0300

    arquivos da aula 02

---
+++ js/global.js
@@ -0,0 +1,7 @@
+const cardNovaLista = document.getElementById('cardNovaLista')
+const btnNovaLista = document.getElementById('btnNovaLista')
+
+btnNovaLista.addEventListener('click' , function() {
+    cardNovaLista.style.display = 'block'
+})
+
